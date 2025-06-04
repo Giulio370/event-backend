6c8c2a3acff957e14707328eadf1af5c0c92309c
Giulio <confentegiulio@gmail.com>
2025-06-04 18:03:44 +0200

    feat: implement event module with image upload and validation 2

--- src/middlewares/multerErrorHandler.js
+++ src/middlewares/multerErrorHandler.js
@@ -2,7 +2,7 @@
 
 module.exports = (err, req, res, next) => {
   if (err) {
-    console.error('❌ Errore multer:', err);
+    console.error('Errore multer:', err);
     return res.status(500).json({ error: err.message || 'Errore upload multer' });
   }
   next();
--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -3,7 +3,7 @@ const { storage } = require('../utils/cloudinary');
 
 const upload = multer({
   storage,
-  limits: { fileSize: 10 * 1024 * 1024 } // limite opzionale: 10MB
+  limits: { fileSize: 10 * 1024 * 1024 } // 10MB
 });
 
 module.exports = upload;
